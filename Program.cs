3eff1f000d04d39ad95b8a5bba0cfeefbdc96713
Ben Underwood <javerikr@gmail.com>
2024-05-23 13:42:07 -0400

    Move .git folder

---
+++ Program.cs
@@ -0,0 +1,1 @@
+﻿string orderStream = "B123,C234,A345,C15,B177,G3003,C235,B179";
--- obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c87f6251fbc07e137bbd7a5ba062b5b1616a4b6a")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
