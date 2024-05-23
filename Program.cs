2493b45bde19ab9992f1c7658093e81c23021528
Ben Underwood <javerikr@gmail.com>
2024-05-23 13:49:32 -0400

    https://learn.microsoft.com/en-us/training/modules/csharp-arrays-operations/8-solution-2

--- Program.cs
+++ Program.cs
@@ -1,1 +1,9 @@
 ﻿string orderStream = "B123,C234,A345,C15,B177,G3003,C235,B179";
+string[] items = orderStream.Split(",");
+Array.Sort(items);
+for (int i = 0; i < items.Length; i++) {
+    if (items[i].Length == 4)
+        Console.WriteLine(items[i]);
+    else
+        Console.WriteLine($"{items[i]}\t- Error");
+}
